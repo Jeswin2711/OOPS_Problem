f50ed4bb6874ab27689d9a55bbccd54650e9fead
Jeswin2711 <jeswin2711@gmail.com>
2022-07-28 13:40:58 +0530

    Added : Statement

---
+++ README.md
@@ -0,0 +1,1 @@
+An Object Oriented Programming Language Program
---
+++ src/main/java/com/assingment/OOPSMain.java
@@ -0,0 +1,8 @@
+package com.assingment;
+
+public class OOPSMain
+{
+    public static void main(String[] args) {
+        System.out.println("Welcome to Object Oriented Practice Problem");
+    }
+}
